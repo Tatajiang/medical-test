238bb81d563fd8652444cabf4fa6a7e5479bf17c
TataJiang <Tatajiang136137@gmail.com>
2020-04-21 16:49:05 +0800

    增加CORSFileter拦截器，修复测试时出现得跨域问题

---
+++ src/main/java/com.jiang.medical/filter/CORSFileter.java
@@ -0,0 +1,40 @@
+package com.jiang.medical.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @description: 运行进行跨域
+ * @author: zhantuo.jiang
+ * @create: 2020-04-15 13:00
+ */
+public class CORSFileter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpServletResponse.setHeader("Access-Control-Allow-Headers","User-Agent,Origin,Cache-Control,Content-type,Date,Server,withCredentials,AccessToken");
+        httpServletResponse.setHeader("Access-Control-Allow-Credentials","true");
+        httpServletResponse.setHeader("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        httpServletResponse.setHeader("Access-Control-Max-Age", "1209600");
+        httpServletResponse.setHeader("Access-Control-Expose-Headers","accesstoken");
+        httpServletResponse.setHeader("Access-Control-Request-Headers","accesstoken");
+        httpServletResponse.setHeader("Expires", "-1");
+        httpServletResponse.setHeader("Cache-Control", "no-cache");
+        httpServletResponse.setHeader("pragma", "no-cache");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
